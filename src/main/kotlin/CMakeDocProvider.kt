cc652c6ffe3e60c10bc00ced57316a770353baab
Jedzia <jed69@gmx.de>
2020-02-28 09:10:18 +0100

    updated to support CLion 2019.3

--- src/main/kotlin/CMakeDocProvider.kt
+++ src/main/kotlin/CMakeDocProvider.kt
@@ -2,87 +2,88 @@ import com.intellij.lang.documentation.AbstractDocumentationProvider
 import com.intellij.notification.Notification
 import com.intellij.notification.NotificationType
 import com.intellij.notification.Notifications
+import com.intellij.openapi.project.ProjectManager
 import com.intellij.psi.PsiElement
-import com.jetbrains.cidr.cpp.toolchains.CPPToolchains
 import com.jetbrains.cidr.cpp.toolchains.CPPEnvironment
+import com.jetbrains.cidr.cpp.toolchains.CPPToolchains
 import com.jetbrains.cmake.psi.CMakeCommandName
 import com.jetbrains.cmake.psi.CMakeElement
 import com.jetbrains.cmake.psi.CMakeLiteral
 import org.asciidoc.intellij.AsciiDoc
 import org.jetbrains.rpc.LOG
 import java.io.IOException
-import java.util.ArrayList
+import java.util.*
 
 class CMakeDocProvider : AbstractDocumentationProvider() {
 
-  private var moduleList = ArrayList<String>()
+    private var moduleList = ArrayList<String>()
 
-  private var propertyList = ArrayList<String>()
+    private var propertyList = ArrayList<String>()
 
-  private var variableList = ArrayList<String>()
+    private var variableList = ArrayList<String>()
 
-  private val asciiDoc by lazy {
-    val tmpdir = createTempDir()
-    AsciiDoc(tmpdir.absolutePath, tmpdir, null, "clion-cmakedocs")
-  }
-
-  fun runCMake(vararg args: String): Process {
-    val cppEnvironment = CPPEnvironment(CPPToolchains.getInstance().getToolchainByNameOrDefault(null)!!)
-    val cmake = cppEnvironment.cMake
-    LOG.info("CDP - Executing CMake: ${cmake!!.executablePath} ${args.joinToString(" ")}")
-    return ProcessBuilder(cmake.executablePath, *args).start()
-  }
-
-  fun <T> withLinesFromCMake(vararg args: String, block: (Sequence<String>) -> T): T? {
-    try {
-      return runCMake(*args).inputStream.bufferedReader().useLines { block(it) }
-    } catch (e: IOException) {
-      e.printStackTrace()
-      Notifications.Bus.notify(Notification("ApplicationName", "CMake Docs", "Unable to run CMake to get docs", NotificationType.ERROR))
+    private val asciiDoc by lazy {
+        val tmpdir = createTempDir()
+        AsciiDoc(ProjectManager.getInstance().createProject(tmpdir.absolutePath, tmpdir.absolutePath), tmpdir, null, "clion-cmakedocs")
     }
-    return null
-  }
 
-  fun getLinesFromCMake(vararg args: String, block: (Sequence<String>) -> Sequence<String> = { it }): List<String> {
-    return withLinesFromCMake(*args) { block(it).toList() } ?: emptyList()
-  }
+    private fun runCMake(vararg args: String): Process {
+        val cppEnvironment = CPPEnvironment(CPPToolchains.getInstance().getToolchainByNameOrDefault(null)!!)
+        val cmake = cppEnvironment.cMake
+        LOG.info("CDP - Executing CMake: ${cmake!!.executablePath} ${args.joinToString(" ")}")
+        return ProcessBuilder(cmake.executablePath, *args).start()
+    }
 
-  fun docForCommand(cmd: String): String? = withLinesFromCMake("--help-command", cmd) { it.joinToString("\n") }
+    private fun <T> withLinesFromCMake(vararg args: String, block: (Sequence<String>) -> T): T? {
+        try {
+            return runCMake(*args).inputStream.bufferedReader().useLines { block(it) }
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Notifications.Bus.notify(Notification("ApplicationName", "CMake Docs", "Unable to run CMake to get docs", NotificationType.ERROR))
+        }
+        return null
+    }
 
-  fun docForLiteral(literal: String): String? {
-    val lit = literal.removeSurrounding("\${", "}") /* remove expansion syntax if present */
-    if (moduleList.isEmpty()) {
-      moduleList.addAll(getLinesFromCMake("--help-module-list"))
+    private fun getLinesFromCMake(vararg args: String, block: (Sequence<String>) -> Sequence<String> = { it }): List<String> {
+        return withLinesFromCMake(*args) { block(it).toList() } ?: emptyList()
     }
-    if (lit in moduleList) {
-      return withLinesFromCMake("--help-module", lit) { it.joinToString("\n") }
-    }
-    if (propertyList.isEmpty()) {
-      propertyList.addAll(getLinesFromCMake("--help-property-list"))
-    }
-    if (lit in propertyList) {
-      return withLinesFromCMake("--help-property", lit) { it.joinToString("\n") }
-    }
-    if (variableList.isEmpty()) {
-      variableList.addAll(getLinesFromCMake("--help-variable-list"))
-    }
-    if (lit in variableList) {
-      return withLinesFromCMake("--help-variable", lit) { it.joinToString("\n") }
-    }
-    return null
-  }
 
-  override fun generateDoc(element: PsiElement?, originalElement: PsiElement?): String? {
-    return if (element == null || element !is CMakeElement) {
-      null
-    } else {
-      when (element) {
-        is CMakeCommandName -> docForCommand(element.name)
-        is CMakeLiteral -> docForLiteral(element.text)
-        else -> null
-      }.let {
-        if (it == null) null else asciiDoc.render(it, emptyList())
-      }
+    private fun docForCommand(cmd: String): String? = withLinesFromCMake("--help-command", cmd) { it.joinToString("\n") }
+
+    private fun docForLiteral(literal: String): String? {
+        val lit = literal.removeSurrounding("\${", "}") /* remove expansion syntax if present */
+        if (moduleList.isEmpty()) {
+            moduleList.addAll(getLinesFromCMake("--help-module-list"))
+        }
+        if (lit in moduleList) {
+            return withLinesFromCMake("--help-module", lit) { it.joinToString("\n") }
+        }
+        if (propertyList.isEmpty()) {
+            propertyList.addAll(getLinesFromCMake("--help-property-list"))
+        }
+        if (lit in propertyList) {
+            return withLinesFromCMake("--help-property", lit) { it.joinToString("\n") }
+        }
+        if (variableList.isEmpty()) {
+            variableList.addAll(getLinesFromCMake("--help-variable-list"))
+        }
+        if (lit in variableList) {
+            return withLinesFromCMake("--help-variable", lit) { it.joinToString("\n") }
+        }
+        return null
     }
-  }
+
+    override fun generateDoc(element: PsiElement?, originalElement: PsiElement?): String? {
+        return if (element == null || element !is CMakeElement) {
+            null
+        } else {
+            when (element) {
+                is CMakeCommandName -> docForCommand(element.name)
+                is CMakeLiteral -> docForLiteral(element.text)
+                else -> null
+            }.let {
+                if (it == null) null else asciiDoc.render(it, emptyList())
+            }
+        }
+    }
 }
